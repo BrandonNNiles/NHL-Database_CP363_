4c1dfbee389067b84f13a4ec71d8d61ba0a69b76
Brandon Niles <brandonniles00@gmail.com>
2022-03-22 14:47:07 -0400

    Login management via JSON

--- scripts/populate-db.py
+++ scripts/populate-db.py
@@ -11,8 +11,6 @@ import requests as req
 import json
 import mysql.connector
 
-db = mysql.connector.connect()
-
 #Methods
 """
 apiConnect(url)
@@ -21,10 +19,10 @@ apiConnect(url)
     Returns the result of connecting to a provided URL
     Prints output based on response
 """
-def apiConnect(url):
-    print("Attempting to reach " + url)
+def apiFetch(url):
+    print("Attempting to fetch from " + url)
     res = req.get(url)
-    print("API connection: " + res.reason)
+    print("API status: " + res.reason)
     return res.json()
 
 """
@@ -35,26 +33,43 @@ dbConnect(hostip, username, password)
     username: string
     Prints output based on response
 """
-def dbConnect(hostip, username, password):
-    print("Attempting to connect to " + hostip + " as " + username)
+def dbConnect(hostip, username, password, db):
+    print("Attempting to connect to database at " + hostip + " as " + username)
     try:
         db = mysql.connector.connect(
             host = hostip,
             user = username,
-            passwd = password
+            passwd = password,
+            database = db
         )
-        print("Successfully connected to " + hostip)
+        print("Successfully connected to " + db.database)
         return db
     except:
         print("Error, failed to connect to DB.")
-        
+
+"""
+getLoginInfo(filepath)
+    filepath: string
+        ->file to look for
+    returns Object for login information
+"""
+def getLoginInfo(filepath):
+    print("Attempting to read file " + filepath)
+    try:
+        f = open(filepath, 'r')
+        data = json.load(f)['login_info']
+        #print("Found: " + str(data)) #You may want to keep this commented out
+        return data['host'], data['user'], data['pass'], data['database']
+    except:
+        print("Error, file: " + filepath + " not found!")
 
 myUrl = "https://statsapi.web.nhl.com/api/v1/teams"
 foundTeams = 0
-for team in apiConnect(myUrl)['teams']:
+for team in apiFetch(myUrl)['teams']:
     for index, entry in team.items():
         if index == 'teamName':
             foundTeams += 1
-            print(entry)
+            #print(entry)
 
-print("Found " + str(foundTeams) + " teams.")
+#print("Found " + str(foundTeams) + " teams.")
+dbConnect(*getLoginInfo('scripts/login.json'))
