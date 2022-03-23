259c460373f2e6d6541436f6bd026269e33c08be
RaminduG <47431201+RaminduG@users.noreply.github.com>
2022-03-23 01:39:06 -0400

    Update example-queries.sql

--- SQL/example-queries.sql
+++ SQL/example-queries.sql
@@ -1,1 +1,4 @@
-/*Put example queries here*/
+/*Key Queries (Blank values to be filled) */
+SELECT * FROM franchises /*Generalized Table of NHL Information */
+SELECT wins, losses, total_goals, head_coach_name FROM team_stats JOIN head_coach_table USING (team_name) WHERE team_name =  '__' /*Find Team Information */
+SELECT first_name, last_name, goals, shots, shooting percentage FROM individual_stats JOIN home_captains USING (player_id) WHERE main_home_captain =  '__' AND team_name = '__'/*Find Captain of a specifc team and their stats*/
